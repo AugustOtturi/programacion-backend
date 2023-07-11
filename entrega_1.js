29f491f282ad8b08e69e6ac4c558dabb92b79f13
Augusto Otturi <augustotturi.dev@gmail.com>
2023-07-11 10:08:21 -0600

    entrega_1 finalizada

--- entrega_1.js
+++ entrega_1.js
@@ -1,28 +1,27 @@
-//!Utilities
+//Utilities
 
 const mathRandom = (array) => {
-    return Math.floor(Math.random() * array.length)
-}
+    return Math.floor(Math.random() * array.length);
+};
 
 const randomId = () => {
     let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i"];
-    let genericId = "";
+    let letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'];
+    let genericId = '';
 
     for (let i = 0; i < 5; i++) {
         if (i <= mathRandom(numbers)) {
             genericId += numbers[mathRandom(numbers)];
         } else {
-            genericId += letter[mathRandom(letter)]
+            genericId += letter[mathRandom(letter)];
         }
     }
-    return genericId
-}
+    return genericId;
+};
 
 let productos = [];
 
-
-//! Clase 
+//Class
 
 class ProductManager {
     constructor(title, description, price, thumbnail, stock) {
@@ -33,41 +32,45 @@ class ProductManager {
         this.code = randomId(); // Utilizo randomId para generar un id al azar
         this.stock = stock;
     }
-    //? getProducts
+
+
     static getProducts() {
         return productos;
     }
 
     static addProduct() {
-        let newProduct = new ProductManager("Producto prueba", "Este es un producto prueba", "$200", "Sin imagen", "25u")
+        let newProduct = new ProductManager(
+            'Producto prueba',
+            'Este es un producto prueba',
+            '$200',
+            'Sin imagen',
+            '25u'
+        );
         if (productos.find((product) => product.code !== newProduct.code)) {
-            return "This product already exists. "
+            return 'This product already exists. ';
         } else {
-            productos.push(newProduct)
-            let output = "";
+            productos.push(newProduct);
+            let output = '';
             for (let el in newProduct) {
-                output += el + ": " + newProduct[el] + "\n";
+                output += el + ': ' + newProduct[el] + '\n';
             }
-            return output
+            return output;
         }
     }
 
     static getProductById(id) {
         const resultado = productos.find((el) => el.code === id);
         if (!resultado) {
-            return `Product with id:${id}, Not foud`
+            return `Product with id:${id}, Not found`;
         } else {
-            return resultado
+            return resultado;
         }
     }
-
-
 }
-
+//Ejecution
 setTimeout(() => {
-    console.log("Starting program");
+    console.log('Starting program');
 }, 1000);
-
 
 setTimeout(() => {
     console.log(ProductManager.getProducts());
@@ -77,17 +80,14 @@ setTimeout(() => {
     console.log(ProductManager.addProduct());
 }, 3000);
 
-
 setTimeout(() => {
-    console.log("Products in inventory:");
+    console.log('Products in inventory:');
     console.log(ProductManager.getProducts());
 }, 4000);
 
 setTimeout(() => {
     console.log(ProductManager.addProduct());
 }, 5000);
-
-
 
 setTimeout(() => {
     console.log(ProductManager.getProductById(123444));
